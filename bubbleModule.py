e7423457d14a524a5833c7d016994ab533e5683c
Hibiki Miyagi <e195710@ie.u-ryukyu.ac.jp>
2021-07-20 14:16:29 +0900

    firstcommit

--- bubbleModule.py
+++ bubbleModule.py
@@ -1,9 +1,9 @@
 # バブルソート（並列化無し）
 def bubble_sort(list):
-    print("ゴブリンが現れた。")
-    new_list = list
-    return new_list
-
+    for i in range(len(list)):
+        for j in range(len(list)-1, i, -1):
+            if list[j-1] > list[j]:
+                list[j-1], list[j] = list[j], list[j-1]
 
 # バブルソート（並列化有り）
 def parallel_bubble_sort(list):
