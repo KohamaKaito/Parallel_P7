97db34b61ebaec9a3f465d917f39e9bdd0105d4d
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-07 08:10:30 +0900

    if __name__="__main__"追加

--- main.py
+++ main.py
@@ -6,47 +6,48 @@ import bucketModule
 import bubbleModule
 import myModule
 
+if __name__ == "__main__":
 
-# ソート対象の配列を準備する
-target_list = myModule.create_list(10)
-
-
-# ソート対象の配列を出力
-print("ソート前の配列↓")
-print(target_list)
+    # ソート対象の配列を準備する
+    target_list = myModule.create_list(10)
 
 
-# マージソート（並列化無し）
-print("マージソート（並列化無し）後の配列↓")
-start = time.time()
-sorted_list01 = mergeModule.merge_sort(target_list)
-stop = time.time()
-print(sorted_list01)
-print('⏱%.3f seconds' % (stop - start))
+    # ソート対象の配列を出力
+    print("ソート前の配列↓")
+    print(target_list)
 
 
-# マージソート（並列化有り）
-print("マージソート（並列化有り）後の配列↓")
-start = time.time()
-sorted_list02 = mergeModule.parallel_merge_sort(target_list)
-stop = time.time()
-print(sorted_list02)
-print('⏱%.3f seconds' % (stop - start))
+    # マージソート（並列化無し）
+    print("マージソート（並列化無し）後の配列↓")
+    start = time.time()
+    sorted_list01 = mergeModule.merge_sort(target_list)
+    stop = time.time()
+    print(sorted_list01)
+    print('⏱%.3f seconds' % (stop - start))
 
 
-# バケットソート（並列化無し）
-print("バケットソート（並列化無し）後の配列↓")
-start = time.time()
-sorted_list03 = bucketModule.bucket_sort(target_list)
-stop = time.time()
-print(sorted_list03)
-print('⏱%.3f seconds' % (stop - start))
+    # マージソート（並列化有り）
+    print("マージソート（並列化有り）後の配列↓")
+    start = time.time()
+    sorted_list02 = mergeModule.parallel_merge_sort(target_list)
+    stop = time.time()
+    print(sorted_list02)
+    print('⏱%.3f seconds' % (stop - start))
 
 
-# バケットソート（並列化有り）
-print("バケットソート（並列化有り）後の配列↓")
-start = time.time()
-sorted_list04 = bucketModule.parallel_bucket_sort(target_list)
-stop = time.time()
-print(sorted_list04)
-print('⏱%.3f seconds' % (stop - start))
+    # バケットソート（並列化無し）
+    print("バケットソート（並列化無し）後の配列↓")
+    start = time.time()
+    sorted_list03 = bucketModule.bucket_sort(target_list)
+    stop = time.time()
+    print(sorted_list03)
+    print('⏱%.3f seconds' % (stop - start))
+
+
+    # バケットソート（並列化有り）
+    print("バケットソート（並列化有り）後の配列↓")
+    start = time.time()
+    sorted_list04 = bucketModule.parallel_bucket_sort(target_list)
+    stop = time.time()
+    print(sorted_list04)
+    print('⏱%.3f seconds' % (stop - start))
